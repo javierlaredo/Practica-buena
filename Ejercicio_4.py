de5c2448ab395eff34cfb72de21e55b7b04fb435
javierlaredo <laredolujan.javier@gmail.com>
2022-09-30 17:41:59 +0200

    Más ejercicios

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -1,8 +1,22 @@
-#Durante la planificación de un proyecto se han acordado una lista de tareas. Para cada una de estas tareas se ha asignado un orden de prioridad (cuanto menor es el número de orden, más prioridad).
+'''Durante la planificación de un proyecto se han acordado una lista de tareas. Para cada una de estas tareas se ha asignado un orden de prioridad (cuanto menor es el número de orden, más prioridad).
+¿Eres capaz de crear una estructura del tipo cola con todas las tareas ordenadas pero sin los números de orden?
 
-#¿Eres capaz de crear una estructura del tipo cola con todas las tareas ordenadas pero sin los números de orden?
+Sugerencia
 
-#Sugerencia
+Para ordenar automáticamente una lista es posible utilizar el método .sort(), deberias probarlo.'''
 
-#Para ordenar automáticamente una lista es posible utilizar el método .sort(), deberias probarlo.
+def ordenar(c):
+    return c['prioridad']
 
+cola = [
+    {'tarea1': 'Redactar', 'prioridad':3},
+    {'tarea2': 'Investigar', 'prioridad':1},
+    {'tarea3': 'Escribir', 'prioridad':2},
+    {'tarea4': 'Jugar', 'prioridad':4}
+
+]
+print(cola)
+cola.sort(key=ordenar)
+n = cola.pop(0)['tarea2']
+
+print(f"Lo más prioritario es: {n}")
